16128f6bdbbbc733d610a1623f1909f35f3e365d
erickzanardoo <erickzanardoo@gmail.com>
2013-10-14 12:57:58 -0300

    fix for buil failed

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ var filePath = process.argv[2];
 var serverPort = parseInt(process.argv[3])
 
 var GITHUB_COMMAND = 'git pull origin master && git checkout ';
-var MAVEN_COMMAND = 'mvn clean install -Dmaven.test.skip.exec=true -Dmaven.compiler.showDeprecation=true | grep WARNING | grep deprecated | wc -l';
+var MAVEN_COMMAND = 'mvn clean install -Dmaven.test.skip.exec=true -Dmaven.compiler.showDeprecation=true > /tmp/warn_out && cat /tmp/warn_out | grep WARNING | grep deprecated | wc -l';
 
 var sys = require('sys')
 var exec = require('child_process').exec;
@@ -25,48 +25,56 @@ var executeCommit = function () {
         console.log(stdout, stderr);
         exec(MAVEN_COMMAND, function(error, stdout, stderr) {
 
-            console.log('WARNINGs founds:' + stdout);
-
-            fs.readFile(filePath, function(err, data) {
-                if (err) {
-                    throw err;
-                }
-                var baseData = JSON.parse(data.toString());
-
-                var baseWarns = baseData.warns;
-                var actualWarns = parseInt(stdout);
-
-                var points = baseWarns - actualWarns;
-                
-                if (points > 0) {
-                    var author = commit.author;
+            if (!error) {
+                console.log('WARNINGs founds:' + stdout);
+                fs.readFile(filePath, function(err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    var baseData = JSON.parse(data.toString());
+    
+                    var baseWarns = baseData.warns;
+                    var actualWarns = parseInt(stdout);
+    
+                    var points = baseWarns - actualWarns;
                     
-                    var member = {};
-                    if (baseData.users[author]) {
-                        member = baseData.users[author];
-                    } else {
-                        member = {
-                            username: author,
-                            kills: 0
-                        };
-                        baseData.users[author] = member;
+                    if (points > 0) {
+                        var author = commit.author;
+                        
+                        var member = {};
+                        if (baseData.users[author]) {
+                            member = baseData.users[author];
+                        } else {
+                            member = {
+                                username: author,
+                                kills: 0
+                            };
+                            baseData.users[author] = member;
+                        }
+    
+                        member.kills += points;
                     }
-
-                    member.kills += points;
-                }
-                baseData.warns = baseWarns - points;
-
-                fs.writeFile(filePath, JSON.stringify(baseData), function (err) {
-                    if (err) return console.log(err);
-                    console.log('Done: ' + commit.id);
-
-                    if (queue.length) {
-                        executeCommit();
-                    } else {
-                        executing = false;
-                    }
+                    baseData.warns = baseWarns - points;
+    
+                    fs.writeFile(filePath, JSON.stringify(baseData), function (err) {
+                        if (err) return console.log(err);
+                        console.log('Done: ' + commit.id);
+    
+                        if (queue.length) {
+                            executeCommit();
+                        } else {
+                            executing = false;
+                        }
+                    });
                 });
-            });
+            } else {
+                console.log('Build failed: ' + commit.id);
+                if (queue.length) {
+                    executeCommit();
+                } else {
+                    executing = false;
+                }
+            }
         });
     });
 };
@@ -88,7 +96,7 @@ http.createServer(function (request, response) {
             for (var i = 0; i < pushCommits.length; i++) {
                 var pushCommit = pushCommits[i];
                 var commit = {
-                    author: pushCommit.author.username,
+                    author: pushCommit.committer.username,
                     id: pushCommit.id
                 };
                 commits.push(commit);
